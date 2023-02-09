28ab94e850dd44913dd3363a2347ceba1087c6de
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-02-09 13:20:11 +0000

    layoutfix

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module WhoseTone
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-    config.time_zone = "Japan"
+    config.time_zone = 'Tokyo'
+    #config.active_record.default_timezone = :local
   end
 end
