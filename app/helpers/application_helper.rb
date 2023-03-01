0f22f8ef4117483cf84aacc42ea1caa13725d7f7
Yusuke Kashiwagi <114566645+Waggy-Music@users.noreply.github.com>
2023-03-01 13:58:04 +0000

    validationFix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,19 @@ def text_url_to_link text
 
   return text
 end
+# ここから追記 これで表示できる<%= error_messages! %>
+    def error_messages!
+      return '' if resource.errors.empty?
+  
+      html = ''
+      resource.errors.full_messages.each do |msg|
+        html += <<-EOF
+          <div class="alert-danger" role="alert">
+            #{msg}
+          </div>
+        EOF
+      end
+  
+      html.html_safe
+    end
 end
